990b204c8d78989de7737e629e5f7c9a13ecab0b
Eric Holzer <eric.holzer.lsn@gmail.com>
2023-10-03 13:46:11 +0200

    Add music

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 const menuHamburger = document.querySelector(".burger-menu");
 const navList = document.querySelector(".nav-list");
-const navBar = document.querySelector(".navbar");
 
 menuHamburger.addEventListener('click', () => {
 	navList.classList.toggle('mobile-menu');
-	navBar.classList.toggle('mobile-menu');
 
 	if (navList.classList.contains('mobile-menu')) {
-		menuHamburger.src = "../images/close_icon.png";
+		menuHamburger.src = "images/close_icon.png";
 		// Deactivate scroll
 		document.body.style.overflow = "hidden";
 	}
 	else {
-		menuHamburger.src = "../images/burger_menu_icon.png";
+		menuHamburger.src = "images/burger_menu_icon.png";
 		// Activate scroll
 		document.body.style.overflow = "auto";
 	}
